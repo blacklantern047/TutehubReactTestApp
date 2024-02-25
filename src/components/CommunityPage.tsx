440e4e25b5a10fd866a23ad06cf2fcb99de796c0
Aryan Bhardwaj <u7504626@anu.edu.au>
2024-02-25 21:47:00 +1100

    first commit

---
+++ src/components/CommunityPage.tsx
@@ -0,0 +1,10 @@
+
+const CommunityPage = () =>{
+    return(
+        <div className="community-page">
+            <h1 className="text-center text-3xl font-bold text-red-600">Community Page</h1>
+        </div>
+    );
+};
+
+export default  CommunityPage;
---
+++ src/components/UserInfo.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const UserInfo = () =>{
+    return(
+        <>
+            <button className="success">Login</button>
+        </>
+    );
+}
+
+export default UserInfo;
